1adc46b134f2cda970d9adf096c75c9d15b1ef41
Aman Shahi <118298784+TheBinaryCoder0@users.noreply.github.com>
2024-12-11 23:40:37 +0530

    Update vector.cpp

--- cpp/vector.cpp
+++ cpp/vector.cpp
@@ -3,27 +3,55 @@
 
 using namespace std;
 
-int main(){
-	vector<int> v1 = {1,2,3,4,5};
+int main() {
+
+	// DEFINING A VECTOR
+
+	vector<int> v1 = {1, 2, 3, 4, 5};
 
 	v1.pop_back();
 	v1.push_back(10);
 
-	int n;
-	cin>>n;
-	vector<int> v2(n);
-	for(int i=0;i<n;i++){
-		cin>>v2[i];
+	// TAKING USER INPUT
+
+	int n1;
+	cin >> n1;
+	vector<int> v2(n1);
+
+	for (int i = 0; i < n1; i++) {
+		cin >> v2[i];
 	}
 
-	for(auto&& i : v1){
-		cout<<i<<" ";
+	for (auto && i : v1) {
+		cout << i << " ";
 	}
 
-	cout<<endl;
+	cout << endl;
 
-	for(auto&& i : v2){
-		cout<<i<<" ";
+	for (auto && i : v2) {
+		cout << i << " ";
 	}
+
+	// DEFINING A 2D VECTOR
+	cout << "Enter n and m: " << endl;
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> v(n, vector<int> (m));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j<m ;j++){
+			cin>>v[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j<m ;j++){
+			cout<<v[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+
+
+
 	return 0;
 }
